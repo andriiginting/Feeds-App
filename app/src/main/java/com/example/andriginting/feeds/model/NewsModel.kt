d7843cdf12b41385c0342939e022cac526113eb2
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-12 13:52:06 +0700

    add news model

---
+++ app/src/main/java/com/example/andriginting/feeds/model/NewsModel.kt
@@ -0,0 +1,12 @@
+package com.example.andriginting.feeds.model
+
+data class NewsModel(val articleAuthor: String,
+                     val articleTitle: String,
+                     val articleDescription: String,
+                     val articleUrl: String,
+                     val articleImageUrl: String,
+                     val articlePublished: String,
+                     val source: SourceArticleModel)
+
+class SourceArticleModel(val id: String,
+                         val name: String)
