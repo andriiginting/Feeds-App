67742dac04dfb10499a857fc996aa5b86a6e7271
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-12 15:55:58 +0700

    add hacker news route

--- app/src/main/java/com/example/andriginting/feeds/network/NewsRoutes.kt
+++ app/src/main/java/com/example/andriginting/feeds/network/NewsRoutes.kt
@@ -1,6 +1,7 @@
 package com.example.andriginting.feeds.network
 
-import com.example.andriginting.feeds.model.NewsResponse
+import com.example.andriginting.feeds.model.hackernews.HackerNewsResponse
+import com.example.andriginting.feeds.model.news.NewsResponse
 import io.reactivex.Observable
 import retrofit2.Response
 import retrofit2.http.GET
@@ -8,8 +9,16 @@ import retrofit2.http.Query
 
 interface NewsRoutes {
 
+    //for news api
     @GET("v2/top-headlines")
     fun getListArticle(@Query("country") countryId: String,
                        @Query("category") newsCategory: String)
             : Observable<Response<NewsResponse>>
+
+    @GET("v0/newstories.json")
+    fun getHackerNewsListId(): Observable<Response<List<Int>>>
+
+    @GET("v0/item/{id}.json")
+    fun getHackerNewsItems(): Observable<Response<List<HackerNewsResponse>>>
+
 }
