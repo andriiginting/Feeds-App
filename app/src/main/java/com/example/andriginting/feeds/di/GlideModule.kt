c97e5a0289470e1dcc87d4afc3b1c5a7aecf5610
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-15 02:53:27 +0700

    add glide module

---
+++ app/src/main/java/com/example/andriginting/feeds/di/GlideModule.kt
@@ -0,0 +1,7 @@
+package com.example.andriginting.feeds.di
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideModule: AppGlideModule()
