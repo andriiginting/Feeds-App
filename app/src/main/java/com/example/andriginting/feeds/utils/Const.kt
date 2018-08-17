3969f5dd220655dcc1f89d2db1b84962da04753a
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-17 23:46:05 +0700

    add const class

---
+++ app/src/main/java/com/example/andriginting/feeds/utils/Const.kt
@@ -0,0 +1,8 @@
+package com.example.andriginting.feeds.utils
+
+object Const {
+    const val HACKER_NEWS_BASE_URL = "https://hacker-news.firebaseio.com/"
+    const val NEWS_API_BASE_URL = "https://newsapi.org/"
+    const val NEWS_DB_NAME = "news"
+    const val HACKER_NEWS_DB_NAME = "hackernews"
+}
