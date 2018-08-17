a82c27dbaa63c7bcf893664e1fba7f32ff9c50ff
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-17 23:46:20 +0700

    add news table class using room

---
+++ app/src/main/java/com/example/andriginting/feeds/repo/local/news/NewsApp.kt
@@ -0,0 +1,16 @@
+package com.example.andriginting.feeds.repo.local.news
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import com.example.andriginting.feeds.utils.Const.NEWS_DB_NAME
+
+@Entity(tableName = "news")
+data class NewsApp(
+        @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") var id: Long = 0,
+        @ColumnInfo(name = "author") var articleAuthor: String,
+        @ColumnInfo(name = "title") var articleTitle: String,
+        @ColumnInfo(name = "url") var articleUrl: String,
+        @ColumnInfo(name = "image_url") var articleImageUrl: String,
+        @ColumnInfo(name = "published_at") var articlePublished: String,
+        @ColumnInfo(name = "description") var articleDescription: String)
