09e54c4c2323a04bd287ae26494720ef1dd0c5e1
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-19 00:29:19 +0700

    [andri] edit app module to inject dep

--- app/src/main/java/com/example/andriginting/feeds/di/AppModule.kt
+++ app/src/main/java/com/example/andriginting/feeds/di/AppModule.kt
@@ -1,21 +1,22 @@
 package com.example.andriginting.feeds.di
 
+import android.arch.lifecycle.ViewModel
 import com.example.andriginting.feeds.network.NetworkClient
+import com.example.andriginting.feeds.network.NewsRoutes
 import com.example.andriginting.feeds.viewmodel.FeedsViewModel
-import org.koin.android.architecture.ext.viewModel
-import org.koin.dsl.module.Module
+import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.applicationContext
+import org.koin.dsl.module.module
 
 class AppModule {
 
-    val feedModule = applicationContext {
-        viewModel {
-            FeedsViewModel()
-        }
-        bean { FeedsViewModel().fetchAllRepo(get(),get()) }
+    val appModule = applicationContext {
 
-        bean { NetworkClient().getNewsServiceRequest() }
-        bean { NetworkClient().getHackerNewsServiceRequest() }
-        factory { NetworkClient() }
+        single { NetworkClient().getNewsServiceRequest() } bind NewsRoutes::class
+
+        single { NetworkClient().getHackerNewsServiceRequest() }
+
+        factory { FeedsViewModel() } bind ViewModel::class
     }
+
 }
