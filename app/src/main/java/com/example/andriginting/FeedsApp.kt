916885ff9416363f53eba908bcb6c0c052d948fb
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-21 01:07:27 +0700

    [andri] build dagger app component in apps class

--- app/src/main/java/com/example/andriginting/FeedsApp.kt
+++ app/src/main/java/com/example/andriginting/FeedsApp.kt
@@ -1,15 +1,26 @@
 package com.example.andriginting
 
 import android.app.Application
+import android.content.Context
 import com.example.andriginting.feeds.di.AppModule
+import com.example.andriginting.feeds.di.dagger.ApplicationComponent
+import com.example.andriginting.feeds.di.dagger.DaggerApplicationComponent
 import org.koin.android.ext.android.startKoin
 
 
 class FeedsApp : Application() {
 
+   private var appsComponent: ApplicationComponent? = null
     override fun onCreate() {
         super.onCreate()
 
         startKoin(this, listOf(AppModule().appModule))
+        appsComponent = DaggerApplicationComponent.create()
+    }
+
+    companion object {
+        fun getApplicationComponent(context: Context): ApplicationComponent?{
+            return (context.applicationContext as FeedsApp).appsComponent
+        }
     }
 }
