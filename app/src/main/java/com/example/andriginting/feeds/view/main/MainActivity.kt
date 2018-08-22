42dfe82803cc1eeb6ab6353dc6bee039527aee17
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-23 01:02:27 +0700

    [andri] add hackernews adapter in mainActivity

--- app/src/main/java/com/example/andriginting/feeds/view/main/MainActivity.kt
+++ app/src/main/java/com/example/andriginting/feeds/view/main/MainActivity.kt
@@ -6,43 +6,60 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.example.andriginting.feeds.R
+import com.example.andriginting.feeds.repo.remote.hackernews.HackerNewsResponse
 import com.example.andriginting.feeds.repo.remote.news.NewsArticleData
 import com.example.andriginting.feeds.viewmodel.FeedsViewModel
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import org.koin.android.architecture.ext.viewModel
-import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
 class MainActivity : AppCompatActivity() {
 
 
-    private val viewModel by inject<FeedsViewModel>()
+    private val viewModel by viewModel<FeedsViewModel>()
 
-    private lateinit var adapter: MainAdapter
+    private lateinit var newsAdapter: MainAdapter
+    private lateinit var hackerAdapter: HackerNewsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar_main)
 
-        viewModel.fetchAllRepo("us","technology")
+        viewModel.fetchAllRepo("us", "technology")
 
-        adapter = MainAdapter( viewModel,this)
-        setupRecycler(adapter)
+        newsAdapter = MainAdapter(viewModel, this)
+        hackerAdapter = HackerNewsAdapter(viewModel, this)
+        setupNewsRecycler(newsAdapter)
+        setupHackerRecycler(hackerAdapter)
     }
 
-    private fun setupRecycler(adapter: MainAdapter){
-        feeds_recyclerview.layoutManager = LinearLayoutManager(this,
-                LinearLayoutManager.VERTICAL,false)
-        feeds_recyclerview.adapter = adapter
+    private fun setupNewsRecycler(adapter: MainAdapter) {
+        news_recyclerview.layoutManager = LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL, false)
+        news_recyclerview.adapter = adapter
         observeNewsData()
     }
 
-    private fun observeNewsData(){
+    private fun setupHackerRecycler(adapter: HackerNewsAdapter) {
+        hacker_recyclerview.layoutManager = LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false)
+        hacker_recyclerview.adapter = adapter
+        observeNewsData()
+    }
+
+
+    private fun observeNewsData() {
         viewModel.getAllNews().observe(this, Observer<List<NewsArticleData>> {
+            //set shimmer gone
+        })
+
+    }
+
+    private fun observeHackerNewsData() {
+        viewModel.getHackerNews().observe(this, Observer<List<HackerNewsResponse>> {
             //set shimmer gone
         })
     }
