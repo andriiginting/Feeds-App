5e6a3fc85d4ae81714f303fcf822bbeee2df0eed
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-23 01:02:06 +0700

    [andri] edit mainadapter class

--- app/src/main/java/com/example/andriginting/feeds/view/main/MainAdapter.kt
+++ app/src/main/java/com/example/andriginting/feeds/view/main/MainAdapter.kt
@@ -10,7 +10,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.ProgressBar
 import android.widget.TextView
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -20,16 +19,13 @@ import com.example.andriginting.feeds.R
 import com.example.andriginting.feeds.di.GlideApp
 import com.example.andriginting.feeds.repo.remote.hackernews.HackerNewsResponse
 import com.example.andriginting.feeds.repo.remote.news.NewsArticleData
-import com.example.andriginting.feeds.view.main.MainAdapter.Companion.bindImageToHolder
 import com.example.andriginting.feeds.viewmodel.FeedsViewModel
 
 class MainAdapter(listViewModel: FeedsViewModel,
                   lifecycleOwner: LifecycleOwner) :
-        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        RecyclerView.Adapter<MainAdapter.NewsViewHolder>() {
 
     private var listNews: ArrayList<NewsArticleData> = ArrayList()
-    private var listHackerNews: ArrayList<HackerNewsResponse> = ArrayList()
-
 
     init {
         listViewModel.getAllNews().observe(lifecycleOwner, Observer<List<NewsArticleData>> { repos ->
@@ -39,62 +35,37 @@ class MainAdapter(listViewModel: FeedsViewModel,
                 notifyDataSetChanged()
             }
         }) //for news api
-
-        listViewModel.getHackerNews().observe(lifecycleOwner, Observer<List<HackerNewsResponse>> { repos ->
-            listHackerNews.clear()
-            if (repos != null) {
-                listHackerNews.addAll(repos)
-                notifyDataSetChanged()
-            }
-        }) //hacker news
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainAdapter.NewsViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.news_feeds_content_item, parent, false)
+        return NewsViewHolder(view)
 
-        return when (viewType) {
-            1 -> {
-                val view = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.news_feeds_content_item, parent, false)
-                NewsViewHolder(view)
-            }
-            2 -> {
-                val view = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.hacker_feeds_content_item, parent, false)
-                HackerNewsHolder(view)
-            }
-            else -> {
-                val view = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.hacker_feeds_content_item, parent, false)
-                HackerNewsHolder(view)
-            }
-        }
-    }
 
-    override fun getItemViewType(position: Int): Int {
-        return if (position % 2 == 0) {
-            0
-        } else {
-            1
-        }
     }
 
     override fun getItemCount(): Int {
-        return listHackerNews.size
+        return listNews.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is HackerNewsHolder) {
-            holder.bindHackerItem(listHackerNews[position])
-        }
+    override fun onBindViewHolder(holder: MainAdapter.NewsViewHolder, position: Int) {
+        holder.bindItem(listNews[position])
 
-        if (holder is NewsViewHolder) {
-            holder.bindItem(listNews[position])
-        }
     }
 
-    companion object {
+    class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var title: TextView = itemView.findViewById(R.id.title_news_article)
+        private var newsImage: ImageView = itemView.findViewById(R.id.image_article)
+
+        fun bindItem(data: NewsArticleData) {
+            title.text = data.articletitle
+            data.articleImageUrl?.let { bindImageToHolder(it, itemView.context, newsImage) }
+
+        }
+
         @SuppressLint("PrivateResource")
-        fun bindImageToHolder(image: String, context: Context, target: ImageView) {
+        private fun bindImageToHolder(image: String, context: Context, target: ImageView) {
             GlideApp.with(context)
                     .load(image)
                     .fitCenter()
@@ -114,34 +85,6 @@ class MainAdapter(listViewModel: FeedsViewModel,
         }
     }
 
-}
-
-
-class HackerNewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private var title: TextView = itemView.findViewById(R.id.title_hacker_article)
-    private var storyType: TextView = itemView.findViewById(R.id.type_hacker_article)
-    private var hackerNewsImage: ImageView = itemView.findViewById(R.id.hacker_image_article)
-
-
-    @SuppressLint("SetTextI18n")
-    fun bindHackerItem(data: HackerNewsResponse) {
-        title.text = data.storiesTitle?.capitalize()
-        storyType.text = data.newsType?.capitalize()
-        bindImageToHolder("https://lelogama.go-jek.com/banner/goride_mobile.jpeg",
-                itemView.context, hackerNewsImage)
-
-    }
-}
-
-class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private var title: TextView = itemView.findViewById(R.id.title_news_article)
-    private var newsImage: ImageView = itemView.findViewById(R.id.image_article)
-
-    fun bindItem(data: NewsArticleData) {
-        title.text = data.articletitle
-        data.articleImageUrl?.let { bindImageToHolder(it, itemView.context, newsImage) }
-
-    }
 
 }
 
