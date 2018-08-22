399e2e66c8d6f27a2e9e55cff170c1b3e5ed8bd7
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-23 01:04:42 +0700

    add isconnected method to check network conn

--- app/src/main/java/com/example/andriginting/feeds/utils/network/NetworkManager.kt
+++ app/src/main/java/com/example/andriginting/feeds/utils/network/NetworkManager.kt
@@ -4,11 +4,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
+import io.reactivex.Observable
 
 class NetworkManager : BroadcastReceiver() {
 
     companion object {
         var connectivityReceiverListener: ConnectivityReceiverListener? = null
+
+        //another approach
+        fun isInternetConnected(context: Context): Observable<Boolean>{
+            val conn: ConnectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork = conn.activeNetworkInfo
+            return Observable.just(activeNetwork != null && activeNetwork.isConnected)
+        }
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
